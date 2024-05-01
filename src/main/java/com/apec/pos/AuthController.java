3cef0da0634b44d01c9f3399b1152a8c120fcafd
Viet Do <viet1234561231@gmail.com>
2024-05-01 13:26:51 +0700

    sua login register

--- src/main/java/com/apec/pos/AuthController.java
+++ src/main/java/com/apec/pos/AuthController.java
@@ -1,5 +1,6 @@
 package com.apec.pos;
 import com.apec.pos.dto.AccountEntityDTO;
+import com.apec.pos.dto.JwtResponse;
 import com.apec.pos.dto.MotelSearch;
 import com.apec.pos.service.AccountService;
 import com.apec.pos.service.CityService;
@@ -44,12 +45,20 @@ public class AuthController {
 
 	@RequestMapping(method = RequestMethod.POST,value = "login")
 	public ResponseEntity login(@RequestBody AccountEntityDTO accountEntityDTO){
+		JwtResponse jwtResponse = accountService.login(accountEntityDTO);
+		if (jwtResponse==null){
+			return ResponseEntity.badRequest().body("Tài khoản hoặc mật khẩu không đúng");
+		}
 		return ResponseEntity.ok(accountService.login(accountEntityDTO));
 	}
 
 	@RequestMapping(method = RequestMethod.POST,value = "register")
 	public ResponseEntity register(@RequestBody AccountEntityDTO accountEntityDTO){
-		return ResponseEntity.ok(accountService.register(accountEntityDTO));
+		JwtResponse jwtResponse = accountService.register(accountEntityDTO);
+		if (jwtResponse==null){
+			return ResponseEntity.badRequest().body("Tài khoản đã tồn tại");
+		}
+		return ResponseEntity.ok(jwtResponse);
 	}
 	@RequestMapping(method = RequestMethod.GET,value = "get-motel-by-id")
 	public ResponseEntity getDetailById(@RequestParam int id){
